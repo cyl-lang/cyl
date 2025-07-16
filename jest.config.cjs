ce2c862aecf9cc741dea6204015a77bd6b6417f3
clxrityy <clxrityxyz@gmail.com>
2025-07-16 18:42:24 -0400

    update

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "node",
-  roots: ["<rootDir>/compiler/src"],
-  testMatch: ["**/*.test.js"],
+  roots: ["<rootDir>/compiler/src", "<rootDir>/design/tests"],
+  testMatch: ["**/*.test.js", "**/*.test.ts", "**/*.test.mts"],
   collectCoverageFrom: ["compiler/src/**/*.js", "!compiler/src/**/*.d.ts"],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
